0faaabe8a5fd0203f1b3f2d5a504629c4a058cb2
minzhang190 <minzhang190@gmail.com>
2020-05-01 06:50:11 +0000

    sug.js

---
+++ sug.js
@@ -0,0 +1,13 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','c9ef9_ga');
+
+c9ef9_ga('create', 'UA-162759593-3', 'auto');
+c9ef9_ga('send', 'pageview');
+
+(function() {
+    var style = document.createElement('style');
+    style.innerHTML = '.ad-tag { display: none; }';
+    document.head.appendChild(style);
+})();
