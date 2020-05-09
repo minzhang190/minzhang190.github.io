669fe8e20b60a0f17c8c5669058571c53b85c610
minzhang190 <minzhang190@gmail.com>
2020-05-09 01:50:38 +0000

    firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-messaging.js');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAitGP_5qbYuEPWXQUsaKgo0-1etrJ3-7Q",
+  authDomain: "hawken-c9ef9.firebaseapp.com",
+  databaseURL: "https://hawken-c9ef9.firebaseio.com",
+  projectId: "hawken-c9ef9",
+  storageBucket: "hawken-c9ef9.appspot.com",
+  messagingSenderId: "239346842884",
+  appId: "1:239346842884:web:b9430603097c18e615a451",
+  measurementId: "G-NQVYEFDL6P"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.messaging();
